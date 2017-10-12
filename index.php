363efcafbc1666601fcb0783e3d7f1cd5579ebe3
Varsha-Ravi <31662158+Varsha-Ravi@users.noreply.github.com>
2017-10-12 12:10:58 -0400

    Add files via upload
    login page styling

--- index.php
+++ index.php
@@ -1,5 +1,70 @@
-<?php
-  require_once('./controllers/login.php');
-  $loginControllerVar = new LoginController();
-  $loginControllerVar->handleCredentials();
-?>
+<!DOCTYPE html>
+<html>
+<style>
+.container{
+	/*border: 3px solid black;*/
+	margin-top: 5%;
+}
+
+</style>
+<head>
+<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+	<?php
+	    session_start();
+		$_SESSION['index']=1;
+	    if (isset($_POST["submit"])) {
+		require 'connect.php'; 		
+		if (!$conn) {
+			die("Conenction failed - ".mysqli_connect_error());
+		} else {
+			echo "connect!<br/>";
+		}
+
+		function validateInputs($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);			
+			return $data;
+		}
+
+		$user_id = $password = "";
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			$user_id = validateInputs($_POST["user_id"]);
+			$password = validateInputs($_POST["password"]);	
+		}
+		
+		$checkUser = "SELECT user_id, password
+			      	  FROM user_info where user_id = '$user_id' AND password = '$password'";
+	    $result = mysqli_query($conn, $checkUser);	
+		var_dump($result);
+	     
+	    if (mysqli_num_rows($result) > 0) {
+			while ($row = $result->fetch_assoc()) {
+				echo "<br>".$row['user_id']." exists";				
+			}		
+		} else {
+			echo "<br/>$user_id does not exist";
+		}
+		
+		$conn->close();
+		}
+
+	?>
+	<div class="login-page">
+		<center><h3>Slack Login</h3></center>
+		<div class="overall row well" align="center">
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">	
+				<div class="form">
+					<input type="text" name="user_id" placeholder="Enter userID"><br>
+					
+					<input type="password" name="password" placeholder="Enter Password"><br>
+					<input type="submit" name="submit" vslue="submit"></input>
+				</div>
+			</form>
+		</div>
+	</div>
+
+</body>
+</html>
