b2b30dd987d50f748dac52787d88aa3a9c6c9350
9848241418 <rkand002@odu.edu>
2017-10-12 03:10:39 -0400

    Home page updated with inseting messages into channels

--- views/home.php
+++ views/home.php
@@ -7,43 +7,62 @@
 
 <head>
 	<meta charset="UTF-8">
-	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
 	<link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
 
 <body>
-	<div class="Main">
-	<!-- 	<div class="messageHolder">
-			<form method="POST" class="messageHolder" style="padding-right: 50%;">
-	    		<input id="textArea" type="text" name="textfield" placeholder="Enter yout text here"/>
-	    		<input type="submit" name="submit"/>
-			</form>
-		</div> -->
-		<div>
+	<div class="container MainDiv">
+		<div class="MessageHome">
+			<div class="MessageDisplay">
 			<?php
 				$homeControlVar1 = new HomeController();
 
 				if(isset($_GET["channel"])){
-					$channelname = $_GET["channel"];	
+					$channelname = $_GET["channel"];
+
 					$channelmessages = $homeControlVar1->viewMessages($channelname);
+					$Msg = "";
+
 					foreach ($channelmessages as $key => $value) {
-						// echo "inside for";
-						// echo $key. '  ' . $value;
+						// print_r($value);
 						foreach ($value as $message) {
-							echo $message;
+							$Msg = $Msg."<div>".$message."</div>";	
 						}
+						echo $Msg;
 					}
 				}
-				else{
-					echo "Rohit";	
-				}
-				
+			?>
+			</div>
+			<div class="MessageEntry">
+				<?php
+					if(isset($_GET["channel"])){
+						$channelname = $_GET["channel"];
+						// echo "hi:". $channelname;
+					}
+					if(isset($_POST["channel"])){
+						$channelname = $_POST["channel"]; 
+					}
+				?>
+				<form method="POST" action="home.php" >
+		    		<input id="textArea" type="text" name="textfield" placeholder="Enter yout text here"/>
+		    		<input type="hidden" name="channel" value="<?php echo $channelname;?>"/>
+		    		<input id="SubmitButton" type="submit" name="submit"/>
+				</form>
+			</div>
+			<?php 
 				
 				// echo $channelname;
-				
-
-			?>
+				$homeControlVar = new HomeController();
+				if(isset($_POST["textfield"])){
+					$channelname = $_POST["channel"];	
+					// echo $channelname;
+					$messageInserted = $_POST["textfield"];
+					// echo $messageInserted;
+					$InsertedMessage = $homeControlVar->insertMessage($channelname,$messageInserted);
+				}
+			 ?>
 		</div>
+		
 		<div class="sideBar">
 			<h2><center>Workspace</center></h2>
 			<div class="SideBarNav">
