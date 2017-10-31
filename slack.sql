db5ceb412dad32a97953b1e8e547b6950105f7fc
Varsha-Ravi <vravi002@odu.edu>
2017-10-31 14:48:16 -0400

    added db

--- slack.sql
+++ slack.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Oct 27, 2017 at 07:53 PM
--- Server version: 5.6.34-log
--- PHP Version: 7.1.5
+-- Host: localhost
+-- Generation Time: Oct 31, 2017 at 07:46 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `slack`
@@ -31,11 +23,11 @@ SET time_zone = "+00:00";
 CREATE TABLE `channel_messages` (
   `channel_id` int(11) NOT NULL,
   `user_id` varchar(20) NOT NULL,
-  `msg_id` int(11) NOT NULL,
-  `message` varchar(2000) NOT NULL,
+  `msg_id` bigint(20) NOT NULL,
+  `message` longtext NOT NULL,
   `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `type` varchar(10) NOT NULL,
-  `dependency` int(11) NOT NULL
+  `type` smallint(6) DEFAULT NULL,
+  `dependency` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -43,7 +35,11 @@ CREATE TABLE `channel_messages` (
 --
 
 INSERT INTO `channel_messages` (`channel_id`, `user_id`, `msg_id`, `message`, `created_time`, `type`, `dependency`) VALUES
-(1, 'porsche@rsprings.gov', 1, 'Welcome to Musicf17 !!!', '2017-10-27 19:34:30', '', 0);
+(3, 'mater@rsprings.gov', 4, 'hey buddy', '2017-10-31 18:41:34', 1, 4),
+(3, 'porsche@rsprings.gov', 1, 'hai everyone <!--', '2017-10-31 18:41:05', 1, 1),
+(4, 'mater@rsprings.gov', 5, 'fine', '2017-10-31 18:41:42', 1, 5),
+(4, 'porsche@rsprings.gov', 2, 'coo', '2017-10-31 18:41:12', 1, 2),
+(5, 'porsche@rsprings.gov', 3, 'yeah', '2017-10-31 18:41:17', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -55,10 +51,30 @@ CREATE TABLE `direct_message` (
   `user1` varchar(20) NOT NULL,
   `user2` varchar(20) NOT NULL,
   `direct_msg_id` int(11) NOT NULL,
-  `direct_message` varchar(2000) NOT NULL,
+  `direct_message` longtext NOT NULL,
   `url` varchar(20) NOT NULL,
   `direct_message_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emoticons`
+--
+
+CREATE TABLE `emoticons` (
+  `emo_id` int(11) NOT NULL,
+  `emo_name` varchar(10) NOT NULL,
+  `image` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `emoticons`
+--
+
+INSERT INTO `emoticons` (`emo_id`, `emo_name`, `image`) VALUES
+(1, 'like', NULL),
+(2, 'dislike', NULL);
 
 -- --------------------------------------------------------
 
@@ -78,13 +94,74 @@ CREATE TABLE `inside_channel` (
 --
 
 INSERT INTO `inside_channel` (`channel_id`, `user_id`, `joined_date`, `left_date`) VALUES
-(1, 'chinga', '2017-10-09 19:08:53', '0000-00-00 00:00:00'),
-(1, 'mater', '2017-10-09 19:10:53', '0000-00-00 00:00:00'),
-(1, 'porsche@rsprings.gov', '2017-10-16 02:03:46', '0000-00-00 00:00:00'),
-(4, 'chinga', '2017-10-09 19:16:18', '0000-00-00 00:00:00'),
-(4, 'mater', '2017-10-09 19:26:18', '0000-00-00 00:00:00'),
-(4, 'porsche@rsprings.gov', '2017-10-16 02:03:46', '0000-00-00 00:00:00'),
-(5, 'mater', '2017-10-10 00:34:55', '0000-00-00 00:00:00');
+(3, 'mater@rsprings.gov', '2017-10-31 05:54:03', '0000-00-00 00:00:00'),
+(3, 'porsche@rsprings.gov', '2017-10-31 17:37:48', '0000-00-00 00:00:00'),
+(4, 'mater@rsprings.gov', '2017-10-31 05:56:50', '0000-00-00 00:00:00'),
+(4, 'porsche@rsprings.gov', '2017-10-31 18:06:54', '0000-00-00 00:00:00'),
+(4, 'singhis', '2017-10-31 05:56:50', '0000-00-00 00:00:00'),
+(4, 'topsecret@agent.org', '2017-10-31 18:06:54', '0000-00-00 00:00:00'),
+(5, 'mater@rsprings.gov', '2017-10-31 05:57:50', '0000-00-00 00:00:00'),
+(5, 'porsche@rsprings.gov', '2017-10-31 18:08:49', '0000-00-00 00:00:00'),
+(5, 'topsecret@agent.org', '2017-10-31 18:08:49', '0000-00-00 00:00:00'),
+(6, 'mater@rsprings.gov', '2017-10-31 06:00:56', '0000-00-00 00:00:00'),
+(6, 'porsche@rsprings.gov', '2017-10-31 18:08:49', '0000-00-00 00:00:00'),
+(6, 'topsecret@agent.org', '2017-10-31 18:08:49', '0000-00-00 00:00:00'),
+(7, 'mater@rsprings.gov', '2017-10-31 18:39:19', '0000-00-00 00:00:00'),
+(7, 'porsche@rsprings.gov', '2017-10-31 18:39:19', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message_type`
+--
+
+CREATE TABLE `message_type` (
+  `ref_id` smallint(6) NOT NULL,
+  `msg_type` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `message_type`
+--
+
+INSERT INTO `message_type` (`ref_id`, `msg_type`) VALUES
+(1, 'post'),
+(2, 'thread'),
+(3, 'reply');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reactions`
+--
+
+CREATE TABLE `reactions` (
+  `msg_id` bigint(20) NOT NULL,
+  `emo_id` int(11) NOT NULL,
+  `users` longtext NOT NULL,
+  `count` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reactions`
+--
+
+INSERT INTO `reactions` (`msg_id`, `emo_id`, `users`, `count`) VALUES
+(1, 1, ';mater@rsprings.gov;', 1),
+(1, 2, ';mater@rsprings.gov;', 1),
+(4, 1, ';mater@rsprings.gov;', 1),
+(4, 2, ';mater@rsprings.gov;', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `threads`
+--
+
+CREATE TABLE `threads` (
+  `reply_id` bigint(20) NOT NULL,
+  `thread_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -112,13 +189,15 @@ CREATE TABLE `user_info` (
 --
 
 INSERT INTO `user_info` (`user_id`, `password`, `first_name`, `last_name`, `display_name`, `what_i_do`, `status`, `phone_number`, `email`, `time_zone`, `skype`, `avatar`) VALUES
-('chinga', '@chick', '', '', '', NULL, '', '', '', '2017-10-09 19:01:54', '', ''),
-('hornet@rsprings.gov', 'doc', 'Doc', 'Hudson', '', NULL, '', '', '', '2017-10-16 01:57:44', '', ''),
-('kachow@rusteze.com', 'mcqueen', 'Lightning', 'McQueen', '', NULL, '', '', '', '2017-10-16 22:56:30', '', ''),
-('mater', '@mater', '', '', '', NULL, '', '', '', '2017-10-09 19:02:42', '', ''),
-('mater@rsprings.gov', 'mater', 'Tow', 'Mater', '', NULL, '', '', '', '2017-10-16 01:52:55', '', ''),
-('porsche@rsprings.gov', 'sally', 'Sally', 'Carrera', '', NULL, '', '', '', '2017-10-16 01:52:55', '', ''),
-('topsecret@agent.org', 'mcmissile', 'Finn', 'McMissile', '', NULL, '', '', '', '2017-10-16 01:57:44', '', '');
+('admin', 'M0n@rch$', 'admin', '', 'admin', NULL, '', '', '', '2017-10-24 13:35:17', '', ''),
+('chinga@cars.com', '@chick', 'Chick', 'Hicks', 'Chick Hicks', NULL, '', '', 'chinga@cars.com', '2017-10-31 18:02:58', '', ''),
+('hornet@rsprings.gov', '@doc', 'Doc', ' Hudson', 'Doc  Hudson', NULL, '', '', 'hornet@rsprings.gov', '2017-10-31 18:03:06', '', ''),
+('iamami', '12345678Aa!', 'Amitabh', 'Bachan', '', NULL, '', '', 'amitab@ac.in', '2017-10-31 18:32:59', '', ''),
+('kachow@rusteze.com', '@mcqueen', 'Lightning', 'McQueen', 'Lightning McQueen', NULL, '', '', 'kachow@rusteze.com', '2017-10-31 18:03:13', '', ''),
+('mater@rsprings.gov', '@mater', 'Tow', 'Mater', 'Tow Mater', NULL, '', '', 'mater@rsprings.gov', '2017-10-31 18:03:25', '', ''),
+('porsche@rsprings.gov', '@sally', 'Sally', 'Carrera', 'Sally Carrera', NULL, '', '', 'porsche@rsprings.gov', '2017-10-31 18:03:38', '', ''),
+('singhis', '123', 'okay', 'singh', '', NULL, '', '', 'singh@s.com', '2017-10-29 23:35:42', '', ''),
+('topsecret@agent.org', '@mcmissile', 'Finn', 'McMissile', 'Finn McMissile', NULL, '', '', 'topsecret@agent.org', '2017-10-31 18:03:45', '', '');
 
 -- --------------------------------------------------------
 
@@ -137,8 +216,15 @@ CREATE TABLE `workspace` (
 --
 
 INSERT INTO `workspace` (`url`, `user_id`, `created_by`) VALUES
-('musicf17.slack.com', 'chinga', 1),
-('musicf17.slack.com', 'mater', 0);
+('musicf17.slack.com', 'admin', 0),
+('musicf17.slack.com', 'chinga@cars.com', 1),
+('musicf17.slack.com', 'hornet@rsprings.gov', 0),
+('musicf17.slack.com', 'iamami', 0),
+('musicf17.slack.com', 'kachow@rusteze.com', 0),
+('musicf17.slack.com', 'mater@rsprings.gov', 0),
+('musicf17.slack.com', 'porsche@rsprings.gov', 0),
+('musicf17.slack.com', 'singhis', 0),
+('musicf17.slack.com', 'topsecret@agent.org', 0);
 
 -- --------------------------------------------------------
 
@@ -151,17 +237,20 @@ CREATE TABLE `workspace_channels` (
   `channel_name` varchar(20) NOT NULL,
   `url` varchar(20) NOT NULL,
   `user_id` varchar(20) NOT NULL,
-  `purpose` varchar(50) NOT NULL
+  `purpose` varchar(50) NOT NULL,
+  `type` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `workspace_channels`
 --
 
-INSERT INTO `workspace_channels` (`channel_id`, `channel_name`, `url`, `user_id`, `purpose`) VALUES
-(1, 'general', 'musicf17.slack.com', 'chinga', ''),
-(4, 'jazz', 'musicf17.slack.com', 'mater', ''),
-(5, 'random', 'musicf17.slack.com', 'mater', '');
+INSERT INTO `workspace_channels` (`channel_id`, `channel_name`, `url`, `user_id`, `purpose`, `type`) VALUES
+(3, 'general', 'musicf17.slack.com', 'mater@rsprings.gov', '', 'Public'),
+(4, 'jazz', 'musicf17.slack.com', 'mater@rsprings.gov', '', 'Private'),
+(5, 'folk', 'musicf17.slack.com', 'mater@rsprings.gov', '', 'Public'),
+(6, 'country', 'musicf17.slack.com', 'mater@rsprings.gov', '', 'Private'),
+(7, 'random', 'musicf17.slack.com', 'porsche@rsprings.gov', 'talk anything', 'Private');
 
 --
 -- Indexes for dumped tables
@@ -173,7 +262,10 @@ INSERT INTO `workspace_channels` (`channel_id`, `channel_name`, `url`, `user_id`
 ALTER TABLE `channel_messages`
   ADD PRIMARY KEY (`channel_id`,`user_id`,`msg_id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `channel_id` (`channel_id`);
+  ADD KEY `channel_id` (`channel_id`),
+  ADD KEY `msg_id` (`msg_id`),
+  ADD KEY `type` (`type`),
+  ADD KEY `dependency` (`dependency`);
 
 --
 -- Indexes for table `direct_message`
@@ -184,11 +276,39 @@ ALTER TABLE `direct_message`
   ADD KEY `user2` (`user2`);
 
 --
+-- Indexes for table `emoticons`
+--
+ALTER TABLE `emoticons`
+  ADD PRIMARY KEY (`emo_id`);
+
+--
 -- Indexes for table `inside_channel`
 --
 ALTER TABLE `inside_channel`
   ADD PRIMARY KEY (`channel_id`,`user_id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `inside_channel_ibfk_1` (`user_id`);
+
+--
+-- Indexes for table `message_type`
+--
+ALTER TABLE `message_type`
+  ADD PRIMARY KEY (`ref_id`);
+
+--
+-- Indexes for table `reactions`
+--
+ALTER TABLE `reactions`
+  ADD PRIMARY KEY (`msg_id`,`emo_id`),
+  ADD KEY `msg_id` (`msg_id`),
+  ADD KEY `emo_id` (`emo_id`);
+
+--
+-- Indexes for table `threads`
+--
+ALTER TABLE `threads`
+  ADD PRIMARY KEY (`reply_id`),
+  ADD KEY `thread_id` (`thread_id`),
+  ADD KEY `reply_id` (`reply_id`);
 
 --
 -- Indexes for table `user_info`
@@ -221,10 +341,15 @@ ALTER TABLE `workspace_channels`
 ALTER TABLE `direct_message`
   MODIFY `direct_msg_id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT for table `emoticons`
+--
+ALTER TABLE `emoticons`
+  MODIFY `emo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT for table `workspace_channels`
 --
 ALTER TABLE `workspace_channels`
-  MODIFY `channel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `channel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- Constraints for dumped tables
 --
@@ -233,37 +358,42 @@ ALTER TABLE `workspace_channels`
 -- Constraints for table `channel_messages`
 --
 ALTER TABLE `channel_messages`
-  ADD CONSTRAINT `channel_messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`),
-  ADD CONSTRAINT `channel_messages_ibfk_2` FOREIGN KEY (`channel_id`) REFERENCES `workspace_channels` (`channel_id`);
+  ADD CONSTRAINT `channel_messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `channel_messages_ibfk_2` FOREIGN KEY (`channel_id`) REFERENCES `workspace_channels` (`channel_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `channel_messages_ibfk_4` FOREIGN KEY (`type`) REFERENCES `message_type` (`ref_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `channel_messages_ibfk_5` FOREIGN KEY (`dependency`) REFERENCES `channel_messages` (`msg_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `direct_message`
 --
 ALTER TABLE `direct_message`
-  ADD CONSTRAINT `direct_message_ibfk_1` FOREIGN KEY (`user1`) REFERENCES `user_info` (`user_id`),
-  ADD CONSTRAINT `direct_message_ibfk_2` FOREIGN KEY (`user2`) REFERENCES `user_info` (`user_id`);
+  ADD CONSTRAINT `direct_message_ibfk_1` FOREIGN KEY (`user1`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `direct_message_ibfk_2` FOREIGN KEY (`user2`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `inside_channel`
 --
 ALTER TABLE `inside_channel`
-  ADD CONSTRAINT `inside_channel_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`),
-  ADD CONSTRAINT `inside_channel_ibfk_2` FOREIGN KEY (`channel_id`) REFERENCES `workspace_channels` (`channel_id`);
+  ADD CONSTRAINT `inside_channel_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `inside_channel_ibfk_2` FOREIGN KEY (`channel_id`) REFERENCES `workspace_channels` (`channel_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reactions`
+--
+ALTER TABLE `reactions`
+  ADD CONSTRAINT `reactions_ibfk_1` FOREIGN KEY (`emo_id`) REFERENCES `emoticons` (`emo_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reactions_ibfk_2` FOREIGN KEY (`msg_id`) REFERENCES `channel_messages` (`msg_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `workspace`
 --
 ALTER TABLE `workspace`
-  ADD CONSTRAINT `workspace_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`);
+  ADD CONSTRAINT `workspace_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `workspace_channels`
 --
 ALTER TABLE `workspace_channels`
-  ADD CONSTRAINT `workspace_channels_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`),
-  ADD CONSTRAINT `workspace_channels_ibfk_2` FOREIGN KEY (`url`) REFERENCES `workspace` (`url`);
-COMMIT;
+  ADD CONSTRAINT `workspace_channels_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `workspace_channels_ibfk_2` FOREIGN KEY (`url`) REFERENCES `workspace` (`url`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
