b8d68896a0c4d52f4d7b23f0971982f149bd1796
Rahul Racha <rrach001@odu.edu>
2017-12-09 21:11:35 -0500

    debug statements for auth

--- views/github_auth_client.php
+++ views/github_auth_client.php
@@ -14,19 +14,24 @@
     $_SESSION['state'] = hash('sha256', microtime(TRUE).rand().$_SERVER['REMOTE_ADDR']);
 
     unset($_SESSION['access_token']);
+    $_SESSION['code'] = "OOPS";
     $githubControlVar->sendRequestParams($_SESSION['state'], $redirect_uri, NULL);
     die();
   }
 
   if (isset($_GET['code']) && !empty($_GET['code'])) {
-    // if(!isset($_GET['state']) || $_SESSION['state'] != $_GET['state']) {
-    //   header('location:login.php');
-    //   die();
-    // }
+    $_SESSION['code'] = "ENTERED CODE";
+    if(!isset($_GET['state']) || $_SESSION['state'] != $_GET['state']) {
+      $_SESSION['code'] = "ERROR STATE";
+      header('location:login.php');
+      die();
+    }
+    $_SESSION['code'] = "YES:".$_GET['code'];
     $githubControlVar->sendRequestParams($_SESSION['state'], $redirect_uri, $_GET['code']);
   }
 
   if (isset($_SESSION['access_token'])) {
+    $_SESSION['code'] = "IN THE USER";
     $userDetails = $githubControlVar->apiRequest();
     echo '<h3>Logged In</h3>';
     echo '<h4>' . $userDetails->name . '</h4>';
